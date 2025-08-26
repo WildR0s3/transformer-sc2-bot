0adde243901603e31137382d3876168f78734122
WildR0s3 <pawel.hajdecki88@gmail.com>
2025-08-26 20:57:05 +0200

    first batch

---
+++ embedding.py
@@ -0,0 +1,6 @@
+# TODO create embedding both coordinates (x, y), acitons and observations
+# I need to select only those acitons and its options that are neccesary for 
+# building drones, overlords, hatcheries, setting worker rally points, moving workers,  maybe control groups
+# model should have 2 phases, 
+#       decode only, actions generation based on past actions and observations, and prediction
+#       encode - getting reward for correct observation prediction
